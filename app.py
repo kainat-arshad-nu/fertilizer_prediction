8aafbf6de4cf7f3308d7305ca0f0ad48a6f189d8
kainat-arshad-nu <98732460+kainat-arshad-nu@users.noreply.github.com>
2022-06-03 23:21:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,23 @@ def predict():
         
     return render_template('index.html', output1=single[0], query1 = inputQuery1, query2 = inputQuery2,query3 = inputQuery3 ,query4 = inputQuery4, query5 = inputQuery5, query6 = inputQuery6)
 
+@app.route("/api/predict/", methods=['POST'])
+def testAPI():
+    inputQuery1 = int(request.form['n'])
+    inputQuery2 = int(request.form['p'])
+    inputQuery3 = int(request.form['k'])
+    inputQuery4 = float(request.form['temp'])
+    inputQuery5 = float(request.form['hum'])
+    inputQuery6 = float(request.form['moisture'])
+
+    model = pickle.load(open("model.sav", "rb"))
+    
+    
+    data = [[inputQuery4, inputQuery5, inputQuery6, inputQuery1, inputQuery3, inputQuery2]]
+
+    single = model.predict(data)
+        
+    return single[0]
+
 if __name__ == "__main__":
     app.run()
